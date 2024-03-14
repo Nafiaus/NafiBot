32118f0426e8437b9fa28c2accd3e412899ecc3b
Cody <46328153+Nafiaus@users.noreply.github.com>
2024-03-14 16:44:46 -0500

    Update TwitchBot.cs
    actually fixed IndexOutOfBoundsException by add flagOffset to be enabled when client-nonce is not found in flag[2]. Now anyone without a client-nonce flag can use bot.

--- TwitchBot.cs
+++ TwitchBot.cs
@@ -44,7 +44,7 @@ namespace Nafi.Twitch
                 if (exit) return null;
 
                 chatRaw = inStream.ReadLine();
-
+                Console.WriteLine(chatRaw);
                 if (chatRaw == null)
                 {
                     Thread.Sleep(100);
@@ -53,198 +53,205 @@ namespace Nafi.Twitch
 
                 if (!chatRaw.Contains("PRIVMSG")) continue;
                 break;
-                }
-                if (chatRaw.Contains("client-nonce")) // fuck nightbot no client-nonce having ass
-                {
-                // Parse Raw Message into words by the ';' character
-                string[] flags = chatRaw.Split(';');
-                // Get Sender Color
-                int colorStartIndex = flags[3].IndexOf("=") + 1;
-                string chatColorHex = flags[3].Substring(colorStartIndex);
-                string chatColor;
-                switch (chatColorHex)
-                {
-                    case "#FF0000":
-                        chatColor = "Red";
-                        break;
-                    case "#0000FF":
-                        chatColor = "Blue";
-                        break;
-                    case "#008000":
-                        chatColor = "Green";
-                        break;
-                    case "#B22222":
-                        chatColor = "FireBrick";
-                        break;
-                    case "#FF7F50":
-                        chatColor = "Coral";
-                        break;
-                    case "#DAA520":
-                        chatColor = "Golden Rod";
-                        break;
-                    case "#8A2BE2":
-                        chatColor = "Blue Violet";
-                        break;
-                    case "#D2691E":
-                        chatColor = "Chocolate";
-                        break;
-                    case "#5F9EA0":
-                        chatColor = "Cadet Blue";
-                        break;
-                    case "#9ACD32":
-                        chatColor = "Yellow Green";
-                        break;
-                    case "#2E8B57":
-                        chatColor = "Sea Green";
-                        break;
-                    case "#1E90FF":
-                        chatColor = "Dodger Blue";
-                        break;
-                    case "#00FF7F":
-                        chatColor = "Spring Green";
-                        break;
-                    case "#FF4500":
-                        chatColor = "Orange Red";
-                        break;
-                    case "#FF69B4":
-                        chatColor = "Hot Pink";
-                        break;
-                    default:
-                        chatColor = "Error";
-                        break;
-                }
-        
-                // Get Subscriber Status
-                int subcriberStartIndex = flags[12].IndexOf("=") + 1;
-                string chatSubscriber;
-                string subscriberBoolean = flags[12].Substring(subcriberStartIndex);
-                if (subscriberBoolean == "0")
-                {
-                    chatSubscriber = "False";
-                }
-                else if (subscriberBoolean == "1")
-                {
-                    chatSubscriber = "True";
-                }
-                else
-                {
-                    chatSubscriber = "Error";
-                }
-        
-                // Get Mod Status
-                int modStartIndex = flags[9].IndexOf("=") + 1;
-                string chatMod;
-                string modBoolean = flags[9].Substring(modStartIndex);
-                if (modBoolean == "0")
-                {
-                    chatMod = "False";
-                }
-                else if (modBoolean == "1")
-                {
-                    chatMod = "True";
-                }
-                else
-                {
-                    chatMod = "Error";
-                }
-        
-                // Get Turbo Status
-                int turboStartIndex = flags[14].IndexOf("=") + 1;
-                string chatTurbo;
-                string turboBoolean = flags[14].Substring(turboStartIndex);
-                if (turboBoolean == "0")
-                {
-                    chatTurbo = "False";
-                }
-                else if (turboBoolean == "1")
-                {
-                    chatTurbo = "True";
-                }
-                else
-                {
-                    chatTurbo = "Error";
-                }
-        
-                // Get Returning Chatter Status
-                int returningChatterStartIndex = flags[10].IndexOf("=") + 1;
-                string chatReturningChatter;
-                string returningChatterBoolean = flags[10].Substring(returningChatterStartIndex);
-                if (returningChatterBoolean == "0")
-                {
-                    chatReturningChatter = "False";
-                }
-                else if (returningChatterBoolean == "1")
-                {
-                    chatReturningChatter = "True";
-                }
-                else
-                {
-                    chatReturningChatter = "Error";
-                }
-        
-                // Get First Message Status
-                int firstMessageStartIndex = flags[6].IndexOf("=") + 1;
-                string chatFirstMessage;
-                string firstMessageBoolean = flags[6].Substring(firstMessageStartIndex);
-                if (firstMessageBoolean == "0")
-                {
-                    chatFirstMessage = "False";
-                }
-                else if (firstMessageBoolean == "1")
-                {
-                    chatFirstMessage = "True";
-                }
-                else
-                {
-                    chatFirstMessage = "Error";
-                }
-        
-                // Get Sender Message ID
-                int msgIdStartIndex = flags[8].IndexOf("=") + 1;
-                string chatMsgId = flags[8].Substring(msgIdStartIndex);
-        
-                // Get Sender Name
-                int nameStartIndex = flags[4].IndexOf("=") + 1;
-                string chatSender = flags[4].Substring(nameStartIndex);
-        
-                // Get User ID
-                int uidStartIndex = flags[15].IndexOf("=") + 1;
-                string chatUID = flags[15].Substring(uidStartIndex);
-        
-                int messageIndexStart = flags[16].IndexOf("#");
-                string chatMessage = flags[16].Substring((messageIndexStart + channel.Length) + 3);
-        
-                // Get Sender Type
-                int userTypeStartIndex = flags[16].IndexOf("=") + 1;
-                int userTypeEndIndex = flags[16].IndexOf(":") - 1;
-                int userTypeLength = userTypeEndIndex - userTypeStartIndex;
-                string chatUserTypeRaw = flags[16].Substring(userTypeStartIndex, userTypeLength);
-                string chatUserType;
-                switch (chatUserTypeRaw)
-                {
-                    case "":
-                        chatUserType = "Normal User";
-                        break;
-                    case "admin":
-                        chatUserType = "Twitch Administrator";
-                        break;
-                    case "global_mod":
-                        chatUserType = "Global Moderator";
-                        break;
-                    case "staff":
-                        chatUserType = "Twitch Employee";
-                        break;
-                    default:
-                        chatUserType = "Error";
-                        break;
-                }
-        
-                return new TwitchChatMessage(chatRaw, chatUserType, chatColor, chatMsgId, chatMod, chatSubscriber, chatTurbo, chatUID, chatReturningChatter, chatFirstMessage, chatSender, chatMessage);
-            } else
-            {
-                return null;
             }
+            int flagOffset = 3;
+            // Parse Raw Message into words by the ';' character
+            string[] flags = chatRaw.Split(';');
+
+            if (flags[2].Contains("client-nonce"))
+            {
+                flagOffset = 0;
+            }
+            else if (flags[2].Contains("color"))
+            {
+                flagOffset = 1;
+            }
+
+            // Get Sender Color
+            int colorStartIndex = flags[3 - flagOffset].IndexOf("=") + 1;
+            string chatColorHex = flags[3 - flagOffset].Substring(colorStartIndex);
+            string chatColor;
+            switch (chatColorHex)
+            {
+                case "#FF0000":
+                    chatColor = "Red";
+                    break;
+                case "#0000FF":
+                    chatColor = "Blue";
+                    break;
+                case "#008000":
+                    chatColor = "Green";
+                    break;
+                case "#B22222":
+                    chatColor = "FireBrick";
+                    break;
+                case "#FF7F50":
+                    chatColor = "Coral";
+                    break;
+                case "#DAA520":
+                    chatColor = "Golden Rod";
+                    break;
+                case "#8A2BE2":
+                    chatColor = "Blue Violet";
+                    break;
+                case "#D2691E":
+                    chatColor = "Chocolate";
+                    break;
+                case "#5F9EA0":
+                    chatColor = "Cadet Blue";
+                    break;
+                case "#9ACD32":
+                    chatColor = "Yellow Green";
+                    break;
+                case "#2E8B57":
+                    chatColor = "Sea Green";
+                    break;
+                case "#1E90FF":
+                    chatColor = "Dodger Blue";
+                    break;
+                case "#00FF7F":
+                    chatColor = "Spring Green";
+                    break;
+                case "#FF4500":
+                    chatColor = "Orange Red";
+                    break;
+                case "#FF69B4":
+                    chatColor = "Hot Pink";
+                    break;
+                default:
+                    chatColor = "Error";
+                    break;
+            }
+
+            // Get Subscriber Status
+            int subcriberStartIndex = flags[12 - flagOffset].IndexOf("=") + 1;
+            string chatSubscriber;
+            string subscriberBoolean = flags[12 - flagOffset].Substring(subcriberStartIndex);
+            if (subscriberBoolean == "0")
+            {
+                chatSubscriber = "False";
+            }
+            else if (subscriberBoolean == "1")
+            {
+                chatSubscriber = "True";
+            }
+            else
+            {
+                chatSubscriber = "Error";
+            }
+
+            // Get Mod Status
+            int modStartIndex = flags[9 - flagOffset].IndexOf("=") + 1;
+            string chatMod;
+            string modBoolean = flags[9 - flagOffset].Substring(modStartIndex);
+            if (modBoolean == "0")
+            {
+                chatMod = "False";
+            }
+            else if (modBoolean == "1")
+            {
+                chatMod = "True";
+            }
+            else
+            {
+                chatMod = "Error";
+            }
+
+            // Get Turbo Status
+            int turboStartIndex = flags[14 - flagOffset].IndexOf("=") + 1;
+            string chatTurbo;
+            string turboBoolean = flags[14 - flagOffset].Substring(turboStartIndex);
+            if (turboBoolean == "0")
+            {
+                chatTurbo = "False";
+            }
+            else if (turboBoolean == "1")
+            {
+                chatTurbo = "True";
+            }
+            else
+            {
+                chatTurbo = "Error";
+            }
+
+            // Get Returning Chatter Status
+            int returningChatterStartIndex = flags[10 - flagOffset].IndexOf("=") + 1;
+            string chatReturningChatter;
+            string returningChatterBoolean = flags[10 - flagOffset].Substring(returningChatterStartIndex);
+            if (returningChatterBoolean == "0")
+            {
+                chatReturningChatter = "False";
+            }
+            else if (returningChatterBoolean == "1")
+            {
+                chatReturningChatter = "True";
+            }
+            else
+            {
+                chatReturningChatter = "Error";
+            }
+
+            // Get First Message Status
+            int firstMessageStartIndex = flags[6 - flagOffset].IndexOf("=") + 1;
+            string chatFirstMessage;
+            string firstMessageBoolean = flags[6 - flagOffset].Substring(firstMessageStartIndex);
+            if (firstMessageBoolean == "0")
+            {
+                chatFirstMessage = "False";
+            }
+            else if (firstMessageBoolean == "1")
+            {
+                chatFirstMessage = "True";
+            }
+            else
+            {
+                chatFirstMessage = "Error";
+            }
+
+            // Get Sender Message ID
+            int msgIdStartIndex = flags[8 - flagOffset].IndexOf("=") + 1;
+            string chatMsgId = flags[8 - flagOffset].Substring(msgIdStartIndex);
+
+            // Get Sender Name
+            int nameStartIndex = flags[4 - flagOffset].IndexOf("=") + 1;
+            string chatSender = flags[4 - flagOffset].Substring(nameStartIndex);
+
+            // Get User ID
+            int uidStartIndex = flags[15 - flagOffset].IndexOf("=") + 1;
+            string chatUID = flags[15 - flagOffset].Substring(uidStartIndex);
+
+            // Get Message
+            int messageIndexStart = flags[16 - flagOffset].IndexOf("#");
+            string chatMessage = flags[16 - flagOffset].Substring((messageIndexStart + channel.Length) + 3);
+
+            // Get Sender Type
+            int userTypeStartIndex = flags[16 - flagOffset].IndexOf("=") + 1;
+            int userTypeEndIndex = flags[16 - flagOffset].IndexOf(":") - 1;
+            int userTypeLength = userTypeEndIndex - userTypeStartIndex;
+            string chatUserTypeRaw = flags[16 - flagOffset].Substring(userTypeStartIndex, userTypeLength);
+            string chatUserType;
+            switch (chatUserTypeRaw)
+            {
+                case "":
+                    chatUserType = "Normal User";
+                    break;
+                case "admin":
+                    chatUserType = "Twitch Administrator";
+                    break;
+                case "global_mod":
+                    chatUserType = "Global Moderator";
+                    break;
+                case "staff":
+                    chatUserType = "Twitch Employee";
+                    break;
+                default:
+                    chatUserType = "Error";
+                    break;
+            }
+
+            return new TwitchChatMessage(chatRaw, chatUserType, chatColor, chatMsgId, chatMod, chatSubscriber, chatTurbo, chatUID, chatReturningChatter, chatFirstMessage, chatSender, chatMessage);
         }
+    
 
         // Write a message to Twitch chat using bot
         public bool Write(string message)
@@ -346,3 +353,4 @@ namespace Nafi.Twitch
         }
     }
 }
+
